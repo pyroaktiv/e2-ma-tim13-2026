907b9722759df53040ed619c7ae05602e8ff4b7f
Igor Ludajic <igor.ludajic@gmail.com>
2026-05-05 20:55:57 +0200

    feat: create GUI for KoZnaZna and Spojnice

---
+++ app/src/main/java/rs/tim13/slagalica/koznazna/ui/KoZnaZnaFragment.kt
@@ -0,0 +1,13 @@
+package rs.tim13.slagalica.koznazna.ui
+
+import android.os.Bundle
+import android.view.View
+import rs.tim13.slagalica.core.ui.BaseFragment
+import rs.tim13.slagalica.databinding.FragmentKoZnaZnaBinding
+
+class KoZnaZnaFragment : BaseFragment<FragmentKoZnaZnaBinding>(FragmentKoZnaZnaBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/rs/tim13/slagalica/spojnice/ui/SpojniceFragment.kt
@@ -0,0 +1,13 @@
+package rs.tim13.slagalica.spojnice.ui
+
+import android.os.Bundle
+import android.view.View
+import rs.tim13.slagalica.core.ui.BaseFragment
+import rs.tim13.slagalica.databinding.FragmentSpojniceBinding
+
+class SpojniceFragment : BaseFragment<FragmentSpojniceBinding>(FragmentSpojniceBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
